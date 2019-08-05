92116dc141efbf2318d47453f35060cd6a7be58c
koviazin.pro <koviazin.pro@gmail.com>
2019-08-05 22:52:38 +0500

    1

--- ArmorCore/Mk1/Workers/ArmorModuleWorker_MotorizedJoints.cs
+++ ArmorCore/Mk1/Workers/ArmorModuleWorker_MotorizedJoints.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RimWorld;
 using Verse;
 
 namespace RimArmorCore.Mk1.Workers
@@ -13,6 +14,17 @@ namespace RimArmorCore.Mk1.Workers
             string result = string.Format("ArmorModuleWorker_MotorizedJoints".Translate(), def.StatAffecter.ToArrayValues());
 
             return result;
+        }
+
+        public override void TransformStat(StatDef statDef, ref float value)
+        {
+            if (!Armor.Active)
+                return;
+
+            if (def.StatAffecter.ContainsKey(statDef))
+            {
+                value += def.StatAffecter[statDef];
+            }
         }
     }
 }
