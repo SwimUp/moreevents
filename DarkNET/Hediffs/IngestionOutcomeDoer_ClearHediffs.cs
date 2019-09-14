b372a6e6e0d4bd49ab321745e5df22e11ab3c987
koviazin.pro <koviazin.pro@gmail.com>
2019-09-14 15:58:01 +0500

    1

---
+++ DarkNET/Hediffs/IngestionOutcomeDoer_ClearHediffs.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace DarkNET.Hediffs
+{
+    public class IngestionOutcomeDoer_ClearHediffs : IngestionOutcomeDoer
+    {
+        public List<HediffDef> Hediffs;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+        {
+            if(Hediffs != null)
+            {
+                foreach(var hediffDef in Hediffs)
+                {
+                    Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(hediffDef);
+                    if(hediff != null)
+                    {
+                        pawn.health.RemoveHediff(hediff);
+                    }
+                }
+            }
+        }
+    }
+}
